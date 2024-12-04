20c7aeed01efeac6aa9c4c1b68f05ca93ac59e4e
CaioVianeda <caio.vianwda@gmail.com>
2024-12-04 18:31:27 -0300

    Initial commit

---
+++ flux-in/src/main/java/com/fluxin/flux_in/FluxInApplication.java
@@ -0,0 +1,13 @@
+package com.fluxin.flux_in;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FluxInApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FluxInApplication.class, args);
+	}
+
+}
